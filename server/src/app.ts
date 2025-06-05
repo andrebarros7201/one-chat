5fdaf13a726d7160f2b6ee9d1b9da6cbaaa125fb
André Barros <andrebarros7201@gmail.com>
2025-06-05 23:18:59 +0100

    create router file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config"; // Load env variables
 
+import {router as messageRouter} from './routes/messageRouter'
+
 const app = express();
+
+app.use("/api/message", messageRouter);
 
 app.listen(process.env.PORT ?? 3000, () => {
   console.log(`Server started on port: ${process.env.PORT ?? 3000}`);
